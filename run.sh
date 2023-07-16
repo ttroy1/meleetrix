11eb06938c6bd5cd0ec687265b59f94e7539a5ec
ttroy1 <126310988+ttroy1@users.noreply.github.com>
2023-07-16 17:25:58 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 sudo pkill -f main.py
 sudo pkill -f index.js
-sudo python3 /home/pi/gitrepo/meleetrix/main.py --led-rows=64 --led-cols=64 --led-gpio-mapping='adafruit-hat' --led-slowdown-gpio=3 &
+sudo python3 /home/pi/meleetrix/main.py --led-rows=64 --led-cols=64 --led-gpio-mapping='adafruit-hat' --led-slowdown-gpio=3 &
 sleep 2
-sudo nohup node /home/pi/gitrepo/meleetrix/index.js &
+sudo nohup node /home/pi/meleetrix/index.js &
